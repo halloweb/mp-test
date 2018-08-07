e53df228a1f2a6f30657d3213beeb9a6cce41906
itw_huzl01 <itw_huzl01@tkmail.com>
2018-08-07 20:08:31 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <p class="title">程序日刊</p>
+    <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="onGotUserInfo">点击授权</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    onGotUserInfo (e) {
+      if (e.mp.detail.userInfo) {
+        wx.setStorageSync('userInfo', e.mp.detail.userInfo)
+        wx.navigateTo({
+          url: '../logs/main'
+        })
+      } else {
+        console.log('授权失败')
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+ .container
+   backgroun-color #298de5
+   .title 
+     font-size 60rpx
+</style>
